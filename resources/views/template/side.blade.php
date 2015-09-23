aa8f1d0b4f1c88cb2add99df216287b435993931
manuel <manuelobarrios@gmail.com>
2015-09-23 19:43:56 -0300

    menus

--- resources/views/template/side.blade.php
+++ resources/views/template/side.blade.php
@@ -9,15 +9,28 @@
 
             <li><a href="{{route('home')}}">Home <span class="sr-only">(current)</span></a></li>
 
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-                    Stock <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="{{route('items')}}">Items</a></li>
 
-                </ul>
-            </li>
+            @foreach(\App\Entities\Menus::all() as $menu)
+
+                @if($menu->main == 0 )
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                            {{$menu->name}} <span class="caret"></span>
+                        </a>
+                        @foreach(\App\Entities\Menus::where('main',$menu->id)->get() as $sub)
+                            <ul class="dropdown-menu">
+                                <li><a href="{{route($sub->routes)}}">{{$sub->name}}</a></li>
+                            </ul>
+                        @endforeach
+                    </li>
+
+
+               @endif
+
+            @endforeach
+
+
+
         </ul>
     </div>
 
