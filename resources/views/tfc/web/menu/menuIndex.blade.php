4af562af6b653105c9b0c7f3aa5b907e576a93a9
jonh06 <jonh_gabriel05@hotmail.com>
2015-12-21 14:10:12 -0300

    cambios del 21

---
+++ resources/views/tfc/web/menu/menuIndex.blade.php
@@ -0,0 +1,30 @@
+<!-- Navigation panel -->
+<nav class="main-nav dark transparent stick-fixed">
+    <div class="full-wrapper relative clearfix">
+        <!-- Logo ( * your text or image into link tag *) -->
+        <div class="nav-logo-wrap local-scroll">
+            <a href="#" class="logo">
+                <img src="images/logo-white.png" alt="" />
+            </a>
+        </div>
+        <div class="mobile-nav">
+            <i class="fa fa-bars"></i>
+        </div>
+
+        <!-- Main Menu -->
+        <div class="inner-nav desktop-nav">
+            <ul class="clearlist">
+                <li><a href="web" class="mn-has-sub active">Home </a></li>
+                <li><a href="web/reglamento">Reglamento</a></li>
+                <li><a href="web/noticias" class="mn-has-sub">Noticias </a></li>
+                <li><a href="web/sedes" class="mn-has-sub">Sedes </a></li>
+                <li><a href="web/galeria" class="mn-has-sub">Galeria de Fotos </a></li>
+                <li><a href="web/inscripcion" class="mn-has-sub">Inscripcion Online </a></li>
+                <li><a href="web/contactanos" class="mn-has-sub">Contactanos </a></li>
+                <li><a href="" class="mn-has-sub"><font color="#c5cc33"><b>Tel.: 4504.9683 </b></font></a></li>
+            </ul>
+        </div>
+        <!-- End Main Menu -->
+    </div>
+</nav>
+<!-- End Navigation panel -->
---
+++ resources/views/tfc/web/template.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>The Futbol Company. Profesionales en Futbol Amaterus.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="css/bootstrap.css" media="screen" />
+    <link rel="stylesheet" href="css/font-awesome.css" media="all" />
+    <link rel="stylesheet" href="css/superfish.css" media="all" />
+    <link rel="stylesheet" href="css/owl.carousel.css" media="all" />
+    <link rel="stylesheet" href="css/owl.theme.css" media="all" />
+    <link rel="stylesheet" href="css/flexslider.css" media="all" />
+    <link rel="stylesheet" href="css/colorbox.css" media="all" />
+    <link rel="stylesheet" href="css/color-options.css" media="all" />
+    <link rel="stylesheet" href="style.css">
+    <link href="css/responsive.css" rel="stylesheet">
+    <script src="js/modernizr.custom.js"></script>
+
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="all" />
+    <![endif]-->
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/skin/black.css" type="text/css" id="colors" />
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,600' rel='stylesheet' type='text/css'>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+</head>
+
+<body class="punica-header-3 punica-footer-3 punica-home-1">
+
+</body>
+</html>
