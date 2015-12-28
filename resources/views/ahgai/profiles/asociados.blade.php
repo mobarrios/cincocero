c86ded06c735b5128d256a7368dd2979f26bfd1f
manuel <manuelobarrios@gmail.com>
2015-12-28 13:25:30 -0300

    profiles ok

---
+++ resources/views/ahgai/profiles/asociados.blade.php
@@ -0,0 +1,12 @@
+@extends('index')
+    @section('content')
+        {{ trans('messages.welcome') }}
+
+        <h1>{{Auth::user()->email}}</h1>
+        <h1>{{Auth::user()->db}}</h1>
+        <h1>{{Auth::user()->Perfil()->first()->profile}}</h1>
+
+
+
+    @endsection
+@stop
