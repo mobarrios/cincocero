e476fffa3373ff6384832f2d9954874d1cb1fc63
jonh06 <jonh_gabriel05@hotmail.com>
2016-03-29 13:53:23 -0300

    index

--- resources/views/motonet/web/index.blade.php
+++ resources/views/motonet/web/index.blade.php
@@ -166,50 +166,199 @@
 </div>
 
 
-
-<section id="contact" class="gray-section contact">
+<section id="destacados" class="gray-section contact">
     <div class="container">
-        <div class="row m-b-lg">
-            <div class="col-lg-12 text-center">
-                <div class="navy-line"></div>
-                <h1>Contact Us</h1>
-                <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p>
+        <h1>Destacados</h1>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/cuatri.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row m-b-lg">
-            <div class="col-lg-3 col-lg-offset-3">
-                <address>
-                    <strong><span class="navy">Company name, Inc.</span></strong><br/>
-                    795 Folsom Ave, Suite 600<br/>
-                    San Francisco, CA 94107<br/>
-                    <abbr title="Phone">P:</abbr> (123) 456-7890
-                </address>
-            </div>
-            <div class="col-lg-4">
-                <p class="text-color">
-                    Consectetur adipisicing elit. Aut eaque, totam corporis laboriosam veritatis quis ad perspiciatis, totam corporis laboriosam veritatis, consectetur adipisicing elit quos non quis ad perspiciatis, totam corporis ea,
-                </p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <a href="mailto:test@email.com" class="btn btn-primary">Send us mail</a>
-                <p class="m-t-sm">
-                    Or follow us on social platform
-                </p>
-                <ul class="list-inline social-icon">
-                    <li><a href="#"><i class="fa fa-twitter"></i></a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                    </li>
-                </ul>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/moto.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 text-center m-t-lg m-b-lg">
-                <p><strong>&copy; 2015 Company Name</strong><br/> consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/cuatri.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/moto.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/cuatri.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/moto.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/cuatri.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="ibox">
+                <div class="ibox-content product-box">
+
+                    <div class="product-imitation">
+                        <img src="assets/web/img/moto.png" alt="foto">
+                    </div>
+                    <div class="product-desc">
+                                <span class="product-price">
+                                    $10200
+                                </span>
+                        <small class="text-muted">Categoría</small>
+                        <a href="#" class="product-name"> Producto</a>
+
+                        <div class="small m-t-xs">
+                            Many desktop publishing packages and web page editors now.
+                        </div>
+                        <div class="m-t text-righ">
+                            <a href="#" class="btn btn-xs btn-outline btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
