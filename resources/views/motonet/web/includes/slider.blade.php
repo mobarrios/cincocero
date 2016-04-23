1010b68e5b0595961e8497c63e0166d20087325c
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-04-23 12:12:50 -0300

    index

--- resources/views/motonet/web/includes/slider.blade.php
+++ resources/views/motonet/web/includes/slider.blade.php
@@ -1,9 +1,9 @@
 <div id="inSlider" class="carousel carousel-fade hidden-xs" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#inSlider" data-slide-to="0" class="active"></li>
-        <li data-target="#inSlider" data-slide-to="1"></li>
-        <li data-target="#inSlider" data-slide-to="2"></li>
-    </ol>
+    {{--<ol class="carousel-indicators">--}}
+        {{--<li data-target="#inSlider" data-slide-to="0" class="active"></li>--}}
+        {{--<li data-target="#inSlider" data-slide-to="1"></li>--}}
+        {{--<li data-target="#inSlider" data-slide-to="2"></li>--}}
+    {{--</ol>--}}
     <div class="carousel-inner" role="listbox">
         <div class="item active">
             <div>
@@ -53,13 +53,13 @@
 
 
     </div>
-    <a class="left carousel-control" href="#inSlider" role="button" data-slide="prev">
-        <span class="arrows fa fa-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#inSlider" role="button" data-slide="next">
-        <span class="arrows fa fa-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+    {{--<a class="left carousel-control" href="#inSlider" role="button" data-slide="prev">--}}
+        {{--<span class="arrows fa fa-chevron-left" aria-hidden="true"></span>--}}
+        {{--<span class="sr-only">Previous</span>--}}
+    {{--</a>--}}
+    {{--<a class="right carousel-control" href="#inSlider" role="button" data-slide="next">--}}
+        {{--<span class="arrows fa fa-chevron-right" aria-hidden="true"></span>--}}
+        {{--<span class="sr-only">Next</span>--}}
+    {{--</a>--}}
 </div>
 
