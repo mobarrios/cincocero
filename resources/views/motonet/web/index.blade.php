69277b353a2f2b3407d7ba7aa7f13727100bcf8b
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-05-04 23:23:08 -0300

    index

--- resources/views/motonet/web/index.blade.php
+++ resources/views/motonet/web/index.blade.php
@@ -45,18 +45,11 @@
 
             </div>
 
-        <div class="container">
+        @include('motonet/web/index/accesorios')
+        @include('motonet/web/index/seguro')
+        @include('motonet/web/index/servicioTecnico')
+        @include('motonet/web/index/metodosDePagos')
 
-            <h1>Métodos de Pago</h1>
 
-            <div class="col-xs-12 text-center">
-                <a href="http://www.todopago.com.ar/promociones-vigentes">
-                    <img class="img-responsive center-block" src="http://www.todopago.com.ar/sites/todopago.com.ar/files/kit_banner_promocional_908x515_3.jpg" alt="" />
-                </a>
-            </div>
-
-            </div>
-
-        </div>
     </section>
 @endsection
---
+++ resources/views/motonet/web/index/metodosDePagos.blade.php
@@ -0,0 +1,13 @@
+<div class="container">
+
+    <h1>Métodos de Pago</h1>
+
+    <div class="col-xs-12 text-center">
+        <a href="http://www.todopago.com.ar/promociones-vigentes">
+            <img class="img-responsive center-block" src="http://www.todopago.com.ar/sites/todopago.com.ar/files/kit_banner_promocional_908x515_3.jpg" alt="" />
+        </a>
+    </div>
+
+</div>
+
+</div>
