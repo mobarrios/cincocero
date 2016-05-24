4a86c9bdb82456b7061e838f0d6fe6ae7cf55b63
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-05-24 11:16:10 -0300

    modal

--- resources/views/motonet/web/index.blade.php
+++ resources/views/motonet/web/index.blade.php
@@ -163,12 +163,13 @@
 
             </div>
 
-        @include('motonet/web/index/accesorios')
-        @include('motonet/web/index/seguro')
-        @include('motonet/web/index/servicioTecnico')
-        @include('motonet/web/index/metodosDePagos')
-        @include('motonet/web/index/sucursales')
+        @include('motonet/web/servicios')
+        {{--@include('motonet/web/index/accesorios')--}}
+        {{--@include('motonet/web/index/seguro')--}}
+        {{--@include('motonet/web/index/servicioTecnico')--}}
+{{--        @include('motonet/web/index/metodosDePagos')--}}
 
+        @include('motonet/web/index/sucursales')
 
     </section>
 
@@ -176,6 +177,83 @@
 <footer>
     <p>&copy;Motonet -  Av. Ricardo balbin 486 - San Miguel - Buenos Aires - 0810-333-6686 </p>
 </footer>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog"  role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Accesorios</h4>
+            </div>
+            <div class="modal-body">
+
+                    <h2>ALARMA DE MOTOS DUOBLOCK FX 330</h2>
+
+                    <h4>Funciones Exclusivas DUOBLOCK FX 330: </h4>
+                    <ul>
+                            <li>
+                                Incorpora 2 (dos) nuevos controles remoto DPN-52, para comando manual y por presencia.
+                            </li>
+                            <li>
+                                Nuevo sensor de desplazamiento (acelerómetro)que detecta cualquier movimiento de la motocicleta
+                            </li>
+                            <li>
+                                Nuevo diseño del módulo de alarma, con ficha de conexión.
+                            </li>
+                            <li>
+                                Tamaño reducido para una mejor instalación
+                            </li>
+                            <li>
+                                Compatible con sensor de intrusión.
+                            </li>
+                            <li>
+                                Activa y desactiva automáticamente por presencia, sin emitir aviso alguno. (modo discreto)
+                            </li>
+                            <li>
+                                Activa y desactiva el sistema de alarma de forma manual, a través del control remoto, con avis visual y luminoso. (modo alerta)
+                            </li>
+                            <li>
+                                Modo asalto por presencia, programable.
+                            </li>
+                            <li>
+                                Modo asalto por control remoto manual, programable.
+                            </li>
+                            <li>
+                                Bloqueo progresivo de marcha, para sus modos asalto.
+                            </li>
+                            <li>
+                                No interfiere el sistema eléctrico de la moto.
+                            </li>
+                            <li>
+                                Mayor resistencia a las interferencias.
+                            </li>
+                            <li>
+                                Bloquea la marcha del motor.
+                            </li>
+                            <li>
+                                Controles Remotos, sirena y modulo central, resistente al agua.
+                            </li>
+                            <li>
+                                Entrada auxiliar de disparo para protección de gavetas o porta objetos.
+                            </li>
+                            <li>
+                                Anulación del sistema de alarma, mediante código secreto.
+                            </li>
+                            <li>
+                                Módulo con menor consumo de energía 2.5 mA
+                            </li>
+                            <li>
+                                Inhibición rápida del control remoto extraviado.
+                            </li>
+                        </ul>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 @yield('js')
 
---
+++ resources/views/motonet/web/servicios.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+
+    <h1>Servicios</h1>
+
+    <div class="col-xs-12">
+        <div class="col-xs-3">
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                accesorios
+            </button>
+        </div>
+
+
+    </div>
+    <!-- Modal -->
+</div>
