d43021d85352ceb80994d761f6e7a4426eb234fc
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-05-24 13:12:38 -0300

    botones servicios

--- resources/views/motonet/web/servicios.blade.php
+++ resources/views/motonet/web/servicios.blade.php
@@ -2,31 +2,31 @@
 
     <h1>Servicios</h1>
 
-    <div class="btn-group col-md-12 hidden-sm hidden-xs" role="group">
-        <button type="button" class="btn btn-info btn-lg col-xs-4 btn-servicios" data-toggle="modal" data-target="#myModal">
+    <div class="hidden-sm hidden-xs">
+        <button type="button" class="btn btn-primary btn-outline btn-mot col-md-2 col-md-offset-2 btn-servicios" data-toggle="modal" data-target="#myModal">
             <i class="icon icon-lock-open-alt"></i>
             Accesorios
         </button>
-        <button type="button" class="btn btn-danger btn-lg col-xs-4 btn-servicios" data-toggle="modal" data-target="#myModal2">
+        <button type="button" class="btn btn-primary btn-outline btn-mot col-md-2 col-md-offset-1 btn-servicios text-sm" data-toggle="modal" data-target="#myModal2">
             <i class="icon icon-user-secret"></i>
             Seguro
         </button>
-        <button type="button" class="btn btn-warning btn-lg col-xs-4 btn-servicios text-sm" data-toggle="modal" data-target="#myModal3">
+        <button type="button" class="btn btn-primary btn-outline btn-mot col-md-2 col-md-offset-1 btn-servicios text-sm" data-toggle="modal" data-target="#myModal3">
             <i class="icon icon-wrench"></i>
             Servicio Técnico
         </button>
     </div>
 
     <div class="visible-xs visible-sm hidden-md hidden-lg">
-        <button type="button" class="btn btn-info btn-block btn-servicios" data-toggle="modal" data-target="#myModal">
+        <button type="button" class="btn btn-primary btn-block btn-servicios" data-toggle="modal" data-target="#myModal">
             <i class="icon icon-lock-open-alt"></i>
             Accesorios
         </button>
-        <button type="button" class="btn btn-danger btn-block col-xs-4 btn-servicios" data-toggle="modal" data-target="#myModal2">
+        <button type="button" class="btn btn-primary btn-block col-xs-4 btn-servicios" data-toggle="modal" data-target="#myModal2">
             <i class="icon icon-user-secret"></i>
             Seguro
         </button>
-        <button type="button" class="btn btn-warning btn-block col-xs-4 btn-servicios text-sm" data-toggle="modal" data-target="#myModal3">
+        <button type="button" class="btn btn-primary btn-block col-xs-4 btn-servicios text-sm" data-toggle="modal" data-target="#myModal3">
             <i class="icon icon-wrench"></i>
             Servicio Técnico
         </button>
