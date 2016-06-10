a72c5ff13f18e93dd9a4631941c2123b00c0be63
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-06-10 12:07:33 -0300

    boludes del index

--- resources/views/motonet/web/index/metodosDePagos.blade.php
+++ resources/views/motonet/web/index/metodosDePagos.blade.php
@@ -2,7 +2,7 @@
 
     <h2>Medios de Pago</h2>
     <hr>
-    <div class="col-xs-12 text-center">
+    <div class="col-xs-12 text-center pb20">
         <a href="http://www.todopago.com.ar/promociones-vigentes">
             <img class="img-responsive center-block" src="http://www.todopago.com.ar/sites/todopago.com.ar/files/kit_banner_promocional_908x515_3.jpg" alt="" />
         </a>
