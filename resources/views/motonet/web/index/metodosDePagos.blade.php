d46238f507e477868bdad5d9c2fd1268a5546820
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-06-10 17:17:33 -0300

    error merge

--- resources/views/motonet/web/index/metodosDePagos.blade.php
+++ resources/views/motonet/web/index/metodosDePagos.blade.php
@@ -2,12 +2,11 @@
 
     <h2>Medios de Pago</h2>
     <hr>
-<<<<<<< HEAD
+
     <div class="col-xs-12 text-center">
         <h3>Todo Pago</h3>
-=======
+
     <div class="col-xs-12 text-center pb20">
->>>>>>> a72c5ff13f18e93dd9a4631941c2123b00c0be63
         <a href="http://www.todopago.com.ar/promociones-vigentes">
             <img class="img-responsive center-block" src="http://www.todopago.com.ar/sites/todopago.com.ar/files/kit_banner_promocional_908x515_3.jpg" alt="" />
         </a>
