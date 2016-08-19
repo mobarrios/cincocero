a9b1f2b0ebfbed4cf42afb3c047bd0118a70ece2
mbarrios <mbarrios@desarrollosocial.gob.ar>
2016-08-19 13:14:41 -0300

    index new

--- resources/views/motonet/web/new/mainPromos.blade.php
+++ resources/views/motonet/web/new/mainPromos.blade.php
@@ -5,18 +5,25 @@
         <div class="col-md-12">
             <div class="wide-banner cnt-strip">
                 <div class="image">
-                    <img class="img-responsive" data-echo="assets/unicase/images/banners/3.jpg" src="assets/unicase/images/blank.gif" alt="">
+                    <img class="img-responsive" data-echo="assets/web/img/slider.png" src="assets/unicase/images/blank.gif" alt="">
 
                 </div>
                 <div class="strip strip-text">
                     <div class="strip-inner">
-                        <h1 class="text-right">20000000 coutas sin interes<br>
-                            <span class="shopping-needs">con todas las tarjetas</span></h1>
+                        <h2 class="text-right">12 coutas sin interes
+                            <br>
+                            <span class="shopping-needs" style=" text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;">con todas las tarjetas.</span>
+                        </h2>
                     </div>
                 </div>
+                <!--
                 <div class="new-label">
-                    <div class="text"></div>
-                </div><!-- /.new-label -->
+                    <div class="text">
+                        Promo
+                    </div>
+                </div>
+                -->
+                <!-- /.new-label -->
             </div><!-- /.wide-banner -->
         </div><!-- /.col -->
 
--- resources/views/motonet/web/new/slider.blade.php
+++ resources/views/motonet/web/new/slider.blade.php
@@ -1,12 +1,13 @@
 <div id="hero">
     <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm">
 
-        <div class="item"  style="width:870px; height:368px; background-image: url(assets/web/img/cuatri.png); ">
+        <div class="item"  style="width:870px; height:368px; background-image: url(assets/web/img/promo1.png); ">
             <div class="container-fluid">
                 <div class="caption bg-color vertical-center text-left">
                     <div class="big-text fadeInDown-1">
-                        <span class="highlight">Nuevo Cuatriciclo HONDA.</span>
-
+                        <div class="big-text fadeInDown-1">
+                            Yamaha<span class="highlight">FIAT</span> 125
+                        </div>
                     </div>
 
                     <div class="button-holder fadeInDown-3">
@@ -16,20 +17,16 @@
             </div><!-- /.container-fluid -->
         </div><!-- /.item -->
 
-        <div class="item" style="background-image: url(assets/web/img/moto.png);">
+        <div class="item" style="background-image: url(assets/web/img/promo2.png);">
             <div class="container-fluid">
                 <div class="caption bg-color vertical-center text-left">
                     <div class="big-text fadeInDown-1">
-                        The new <span class="highlight">imac</span> for you
+                         <span class="highlight"> HONDA XR</span> 150
                     </div>
 
-                    <div class="excerpt fadeInDown-2 hidden-xs">
 
-                        <span>21.5-Inch Now Starting At $1099 </span>
-                        <span>27-Inch Starting At $1799</span>
-                    </div>
                     <div class="button-holder fadeInDown-3">
-                        <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Shop Now</a>
+                        <a href="index.php?page=single-product" class="btn-lg btn btn-uppercase btn-primary shop-now-button">Descubrilo</a>
                     </div>
                 </div><!-- /.caption -->
             </div><!-- /.container-fluid -->
