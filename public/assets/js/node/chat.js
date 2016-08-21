0b3e14cd615028bd2c86119344e6280310a11ebf
mbarrios <mbarrios@desarrollosocial.gob.ar>
2016-08-21 00:14:12 -0300

    node

--- public/assets/js/node/chat.js
+++ public/assets/js/node/chat.js
@@ -2,7 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
@@ -12,6 +14,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(2222, function(){
+    console.log('listening on *:2222');
 });
