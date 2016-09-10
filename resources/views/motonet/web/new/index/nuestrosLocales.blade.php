d2a0379776874ac2062239f28c6d78bd370752f4
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-09-10 08:35:51 -0300

    include de scripts de google

--- resources/views/motonet/web/new/index/nuestrosLocales.blade.php
+++ resources/views/motonet/web/new/index/nuestrosLocales.blade.php
@@ -1,20 +1,20 @@
 <section class="section outer-bottom-vs wow fadeInUp" id="sucursales">
-    <h3 class="section-title">Puntos de entrega</h3>
+    <h3 class="section-title">Puntos de entrega:</h3>
+
     <div class="blog-slider-container outer-top-xs">
         <div class="owl-carousel blog-slider custom-carousel">
-
             @foreach($branches as $branch)
                 <div class="item text-center">
                     <div class="blog-post">
                         <div class="blog-post-image">
                             <div class="image">
-                                <a href="blog.html"><img data-echo="{{$branch->images->first()->image or ''}}" width="270" height="135" src="{{$branch->images->first()->image or ''}}" alt=""></a>
+                                <a href=""><img data-echo="{!! $branch->images->first()->image or ''!!}" width="270" height="135" src="{!! $branch->images->first()->image or ''!!}" alt=""></a>
                             </div>
                         </div><!-- /.blog-post-image -->
 
                         <div class="blog-post-info ">
-                            <h3 class="name"><a href="#">{{$branch->name}}</a></h3>
-                            <span class="info">{{$branch->address}}</span>
+                            <h3 class="name"><a href="#">{{ $branch->name }}</a></h3>
+                            <span class="info">{!! $branch->address !!}</span>
                             <p class="text"></p>
                             <a href="" >
                                 <span class="icon fa-stack fa-lg">
@@ -28,7 +28,6 @@
                     </div><!-- /.blog-post -->
                 </div><!-- /.item -->
             @endforeach
-
 
         </div><!-- /.owl-carousel -->
     </div><!-- /.blog-slider-container -->
