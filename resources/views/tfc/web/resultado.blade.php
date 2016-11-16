e85bea5f41264d7585129d902c0cf767838edb46
jonatan jorge <jonh_gabriel05@hotmail.com>
2016-11-16 14:02:41 -0300

    sacar teams

--- resources/views/tfc/web/resultado.blade.php
+++ resources/views/tfc/web/resultado.blade.php
@@ -38,7 +38,7 @@
 
                           <tr>
                               <td colspan="2" align="center" class="active"><img src="assets/web/images/teamDefault.png" width="35" height="35" alt=""/></td>
-                              <td colspan="2" align="center" class="active"><a href="{{route('equipo',[$categoriaActual->id,$faseActual->id,$tab->teams->id or 'jj'])}}">{!! $tab->teams->name or '' !!}</a></td>
+                              <td colspan="2" align="center" class="active"><a href="{{route('equipo',[$categoriaActual->id,$faseActual->id,'jj'])}}"></a></td>
                               <td colspan="2" align="center" class="active">{!! $tab->pts or '0' !!}</td>
                               <td colspan="2" align="center" class="active">{!! $tab->pj or '0' !!}</td>
                               <td colspan="2" align="center" class="active">{!! $tab->pg or '0' !!}</td>
