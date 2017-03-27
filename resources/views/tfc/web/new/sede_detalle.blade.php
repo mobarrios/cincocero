c882702c1841ad8ca278068be0a88f18a6568252
jonatan jorge <jonh_gabriel05@hotmail.com>
2017-03-27 10:12:57 -0300

    prueba mail

--- resources/views/tfc/web/new/sede_detalle.blade.php
+++ resources/views/tfc/web/new/sede_detalle.blade.php
@@ -37,9 +37,7 @@
                                 <div class="contact-form-wrap uk-flex">
                                     <div class="uk-container uk-container-center uk-flex-item-1">
                                         <div class="uk-grid  uk-grid-collapse uk-flex-item-1 uk-height-1-1 uk-nbfc">
-                                            <div id="map" class="center-block" data-direccion="{!! $sedes->address !!}" style="width:800px;height:600px;"></div>
-                                         {{--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.4312651174223!2d-58.52500968417153!3d-34.74472757248102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccfa5acf25ef5%3A0xb86ad5063a8acb09!2sSETIA!5e0!3m2!1ses-419!2sar!4v1478519507232" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>--}}
-
+                                            <div id="map_canvas" class="center-block" data-direccion="{!! $sedes->address !!}" style="width:800px;height:600px;"></div>
 
                                          </div>
                                     </div>
@@ -52,4 +50,79 @@
             </div>
         </div>
         <br><br>
+@endsection
+
+@section('js')
+    <script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
+    <script>
+//        window.map = false;
+//
+//
+//
+//        function initialize(){
+//            var myLatlng = new google.maps.LatLng(-34.595034,-58.51029510000001);
+//
+//            var mapOptions = {
+//                zoom:16,
+//                center: myLatlng,
+//                mapTypeId: google.maps.MapTypeId.ROADMAP,
+//                scrollwheel: false,
+//
+//                streetViewControl:false,
+//                overviewMapControl:false,
+//                mapTypeControl:false
+//
+//            };
+//
+//            window.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//
+//        }
+//
+//        google.maps.event.addDomListener(window, 'load', initialize);
+
+
+        var address = $('#map_canvas').attr('data-direccion');
+
+
+
+        // Creamos el Objeto Geocoder
+        var geocoder = new google.maps.Geocoder();
+        // Hacemos la petición indicando la dirección e invocamos la función
+        // geocodeResult enviando todo el resultado obtenido
+        if(geocoder.geocode({ 'address': address}, geocodeResult))
+            geocoder.geocode({ 'address': address}, geocodeResult);
+        else{
+            var coords = address.split(",");
+            var lat = parseFloat(coords[0]);
+            var lng = parseFloat(coords[1]);
+            var latlng = new google.maps.LatLng(lat, lng);
+            geocoder.geocode({ 'latLng': latlng}, geocodeResult);
+        }
+
+        function geocodeResult(results, status) {
+            // Verificamos el estatus
+            if (status == 'OK') {
+                // Si hay resultados encontrados, centramos y repintamos el mapa
+                // esto para eliminar cualquier pin antes puesto
+                var mapOptions = {
+                    zoom: 16,
+                    center: results[0].geometry.location,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map($("#map_canvas").get(0), mapOptions);
+                // fitBounds acercará el mapa con el zoom adecuado de acuerdo a lo buscado
+//                map.fitBounds(results[0].geometry.viewport);
+                // Dibujamos un marcador con la ubicación del primer resultado obtenido
+                var markerOptions = { position: results[0].geometry.location }
+                var marker = new google.maps.Marker(markerOptions);
+                marker.setMap(map);
+            } else {
+                // En caso de no haber resultados o que haya ocurrido un error
+                // lanzamos un mensaje con el error
+//                alert("No se encontrá la ubicación");
+            }
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 @endsection
