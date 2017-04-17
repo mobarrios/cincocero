afcc1ee03bfa2dc057641c1b4edf0c674963a954
Fernando Alfonso <fernando@keepers.com.ar>
2017-04-17 16:33:47 -0300

    cambios del index css e imagenes

---
+++ resources/views/tfc/web/new/index.blade-viejo.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en-gb" class="uk-height-1-1">
+
+<head>
+    <base href="{!! asset('assets/web/new/asd') !!}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Futbol Company. Profesionales en Futbol Amateurs.</title>
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" type="text/css">
+    <link href="images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+    <link href="css/akslider.css" rel="stylesheet" type="text/css" />
+    <link href="css/donate.css" rel="stylesheet" type="text/css" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" />
+    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/mootools/1.3.1/mootools-yui-compressed.js'></script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+</head>
+
+<body class="uk-vertical-align uk-text-center tt-offline">
+    <div class="offline-logo">
+        <div class="wrap"></div>
+    </div>
+    <div class="uk-panel uk-width-1-1 uk-panel-box uk-vertical-align-middle message-offline uk-container-center">
+        <div id="system-message-container"></div>
+
+        <img src="../../../images/logo-white_nuevo.png" alt="The Futbol Company" width="50%">
+        <br><br><br><br><font size="4"><p>Selecciona <span class="mes-color">tu Torneo.</span></p></font><br>
+
+
+
+
+        <table align="center" width="50%" border="0" class="responsive">
+          <tbody>
+            <tr>
+              <td><a href="{!! route('new.torneos',[5,27]) !!}"><img src="../../../images/copa01.png" alt="The Futbol Company" class="img-polaroid"></a></td>
+              <td><a href="{!! route('new.torneos',[5,25]) !!}"><img src="../../../images/copa02.png" alt="The Futbol Company" class="img-polaroid"></a></td>
+              <td><a href="{!! route('new.torneos',[5,24]) !!}"><img src="../../../images/copa03.png" alt="The Futbol Company" class="img-polaroid"></a></td>
+              <td><a href="{!! route('new.torneos',[5,26]) !!}"><img src="../../../images/copa04.png" alt="The Futbol Company" class="img-polaroid"></a></td>
+              <td><a href="{!! route('new.torneos',[6,29]) !!}"><img src="../../../images/copa05.png" alt="The Futbol Company" class="img-polaroid"></a></td>
+              <td><a href="{!! route('new.torneos',[5,28]) !!}"><img src="../../../images/copa06.png" alt="The Futbol Company" class="img-polaroid"></a></td>
+            </tr>
+            <tr>
+              <td class="uk-h4" style="font-size:14px;">La Liga</td>
+              <td class="uk-h4" style="font-size:14px;">Lega Calcio</td>
+              <td class="uk-h4" style="font-size:14px;">Premier League</td>
+              <td class="uk-h4" style="font-size:14px;">Bundesliga</td>
+              <td class="uk-h4" style="font-size:14px;">Le Championnat</td>
+              <td class="uk-h4" style="font-size:14px;">TFC Champions League</td>
+            </tr>
+          </tbody>
+        </table>
+
+</div>
+    </div>
+    
+   
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/uikit.js"></script>
+<script type="text/javascript" src="js/SimpleCounter.js"></script>
+<script type="text/javascript" src="js/components/grid.js"></script>
+<script type="text/javascript" src="js/components/slider.js"></script>
+<script type="text/javascript" src="js/components/slideshow.js"></script>
+<script type="text/javascript" src="js/components/slideset.js"></script>
+<script type="text/javascript" src="js/components/sticky.js"></script>
+<script type="text/javascript" src="js/components/lightbox.js"></script>
+<script type="text/javascript" src="js/components/accordion.js"></script>
+<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+
+<script type="text/javascript" src="js/theme.js"></script>
+</body>
+
+
+</html>
--- resources/views/tfc/web/new/index.blade.php
+++ resources/views/tfc/web/new/index.blade.php
@@ -1,75 +1,47 @@
-<!DOCTYPE html>
-<html lang="en-gb" class="uk-height-1-1">
 
+<!-- Tutorial URL: http://sixrevisions.com/css/responsive-background-image/ -->
+
+<!doctype html>
+<html lang="en">
 <head>
-    <base href="{!! asset('assets/web/new/asd') !!}">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>The Futbol Company. Profesionales en Futbol Amateurs.</title>
-    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" type="text/css">
-    <link href="images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-    <link href="css/akslider.css" rel="stylesheet" type="text/css" />
-    <link href="css/donate.css" rel="stylesheet" type="text/css" />
-    <link href="css/theme.css" rel="stylesheet" type="text/css" />
-    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/mootools/1.3.1/mootools-yui-compressed.js'></script>
+<base href="{!! asset('assets/web/new/asd') !!}">
+<meta charset="utf-8">
+<title>The Futbol Company. Profesionales en Futbol Amateurs.</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="author" content="Six Revisions">
+  <meta name="description" content="How to use the CSS background-size property to make an image fully span the entire viewport.">
+  <link rel="icon" href="http://sixrevisions.com/favicon.ico" type="image/x-icon" />
+  <link href="http://fonts.googleapis.com/css?family=Kotta+One|Cantarell:400,700" rel="stylesheet" type="text/css">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
 
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+  <!-- Not required: presentational-only.css only contains CSS for prettifying the demo -->
+  <link rel="stylesheet" href="css/presentational-only.css">
+
+  <!-- responsive-full-background-image.css stylesheet contains the code you want -->
+  <link rel="stylesheet" href="css/responsive-full-background-image.css">
+  
+  <!-- Not required: jquery.min.js and presentational-only.js is only used to demonstrate scrolling behavior of the viewport  -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="css/presentational-only.js"></script>
 </head>
-
-<body class="uk-vertical-align uk-text-center tt-offline">
-    <div class="offline-logo">
-        <div class="wrap"></div>
+<body>
+  <nav class="navbar">
+    <div class="inner">
+      <img src="../../../images/logo-white_nuevo.png" width="50%">
     </div>
-    <div class="uk-panel uk-width-1-1 uk-panel-box uk-vertical-align-middle message-offline uk-container-center">
-        <div id="system-message-container"></div>
+  </nav>
 
-        <img src="../../../images/logo-white_nuevo.png" alt="The Futbol Company" width="50%">
-        <br><br><br><br><font size="4"><p>Selecciona <span class="mes-color">tu Torneo.</span></p></font><br>
+  <header class="container">
 
+    <section class="content">
+      <p class="sub-title"><strong>Selecciona tu Torneo</strong> </p>
+      <p><a href="{!! route('new.torneos',[5,27]) !!}"><img src="../../../images/copa01.png"></a><a href="{!! route('new.torneos',[5,25]) !!}"><img src="../../../images/copa02.png"></a><a href="{!! route('new.torneos',[5,24]) !!}"><img src="../../../images/copa03.png"></a><a href="{!! route('new.torneos',[5,26]) !!}"><img src="../../../images/copa04.png"></a><a href="{!! route('new.torneos',[6,29]) !!}"><img src="../../../images/copa05.png"></a><a href="{!! route('new.torneos',[5,28]) !!}"><img src="../../../images/copa06.png"></a></p>
+     
 
-
-
-        <table align="center" width="50%" border="0" class="table">
-          <tbody>
-            <tr>
-              <td><a href="{!! route('new.torneos',[5,27]) !!}"><img src="../../../images/copa01.png" alt="The Futbol Company"></a></td>
-              <td><a href="{!! route('new.torneos',[5,25]) !!}"><img src="../../../images/copa02.png" alt="The Futbol Company"></a></td>
-              <td><a href="{!! route('new.torneos',[5,24]) !!}"><img src="../../../images/copa03.png" alt="The Futbol Company"></a></td>
-              <td><a href="{!! route('new.torneos',[5,26]) !!}"><img src="../../../images/copa04.png" alt="The Futbol Company"></a></td>
-              <td><a href="{!! route('new.torneos',[6,29]) !!}"><img src="../../../images/copa05.png" alt="The Futbol Company"></a></td>
-              <td><a href="{!! route('new.torneos',[5,28]) !!}"><img src="../../../images/copa06.png" alt="The Futbol Company"></a></td>
-            </tr>
-            <tr>
-              <td class="uk-h4" style="font-size:14px;">La Liga</td>
-              <td class="uk-h4" style="font-size:14px;">Lega Calcio</td>
-              <td class="uk-h4" style="font-size:14px;">Premier League</td>
-              <td class="uk-h4" style="font-size:14px;">Bundesliga</td>
-              <td class="uk-h4" style="font-size:14px;">Le Championnat</td>
-              <td class="uk-h4" style="font-size:14px;">TFC Champions League</td>
-            </tr>
-          </tbody>
-        </table>
-
-</div>
-    </div>
-    
-   
-
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/uikit.js"></script>
-<script type="text/javascript" src="js/SimpleCounter.js"></script>
-<script type="text/javascript" src="js/components/grid.js"></script>
-<script type="text/javascript" src="js/components/slider.js"></script>
-<script type="text/javascript" src="js/components/slideshow.js"></script>
-<script type="text/javascript" src="js/components/slideset.js"></script>
-<script type="text/javascript" src="js/components/sticky.js"></script>
-<script type="text/javascript" src="js/components/lightbox.js"></script>
-<script type="text/javascript" src="js/components/accordion.js"></script>
-<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
-
-<script type="text/javascript" src="js/theme.js"></script>
+      
+    </section>
+  </header>
 </body>
-
-
 </html>
