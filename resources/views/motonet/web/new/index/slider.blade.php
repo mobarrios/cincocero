734335b63ad1e77d6207fc942197377fb990fbd2
Manuel Barrios <mbarrios@desarrollosocial.gob.ar>
2017-08-24 16:42:25 -0300

    slider new

--- resources/views/motonet/web/new/index/slider.blade.php
+++ resources/views/motonet/web/new/index/slider.blade.php
@@ -1,22 +1,22 @@
 <div id="hero">
     <div id="owl-main" class="owl-carousel owl-inner-nav owl-ui-sm h-auto">
 
-        <div class="item h-auto">
+        {{--<div class="item h-auto">--}}
         {{--<div class="item"  style="width:870px; height:368px; background: url(assets/web/img/promo1.png) no-repeat; background-size: cover;">--}}
+            {{--<div class="container-fluid">--}}
+                {{--<img src="assets/web/img/promo2.png" alt="" class="img-responsive">--}}
+                {{--<div class="caption bg-color vertical-center text-left">--}}
+                    {{--<div class="big-text fadeInDown-1">--}}
+
+                    {{--</div>--}}
+
+                {{--</div><!-- /.caption -->--}}
+            {{--</div><!-- /.container-fluid -->--}}
+        {{--</div><!-- /.item -->--}}
+
+        <div class="item h-auto">
             <div class="container-fluid">
                 <img src="assets/web/img/promo1.png" alt="" class="img-responsive">
-                <div class="caption bg-color vertical-center text-left">
-                    <div class="big-text fadeInDown-1">
-
-                    </div>
-
-                </div><!-- /.caption -->
-            </div><!-- /.container-fluid -->
-        </div><!-- /.item -->
-
-        <div class="item h-auto">
-            <div class="container-fluid">
-                <img src="assets/web/img/promo2.png" alt="" class="img-responsive">
                 <div class="caption bg-color vertical-center text-left">
                     <div class="big-text fadeInDown-1">
                     </div>
@@ -26,6 +26,39 @@
             </div><!-- /.container-fluid -->
         </div><!-- /.item -->
 
+        <div class="item h-auto">
+            <div class="container-fluid">
+                <img src="assets/web/img/slider3.jpg" alt="" class="img-responsive">
+                <div class="caption bg-color vertical-center text-left">
+                    <div class="big-text fadeInDown-1">
+                    </div>
+                    <div class="button-holder fadeInDown-3">
+                    </div>
+                </div><!-- /.caption -->
+            </div><!-- /.container-fluid -->
+        </div><!-- /.item -->
+        <div class="item h-auto">
+            <div class="container-fluid">
+                <img src="assets/web/img/slider4.jpg" alt="" class="img-responsive">
+                <div class="caption bg-color vertical-center text-left">
+                    <div class="big-text fadeInDown-1">
+                    </div>
+                    <div class="button-holder fadeInDown-3">
+                    </div>
+                </div><!-- /.caption -->
+            </div><!-- /.container-fluid -->
+        </div><!-- /.item -->
+        <div class="item h-auto">
+            <div class="container-fluid">
+                <img src="assets/web/img/slider5.jpg" alt="" class="img-responsive">
+                <div class="caption bg-color vertical-center text-left">
+                    <div class="big-text fadeInDown-1">
+                    </div>
+                    <div class="button-holder fadeInDown-3">
+                    </div>
+                </div><!-- /.caption -->
+            </div><!-- /.container-fluid -->
+        </div><!-- /.item -->
 
 
     </div><!-- /.owl-carousel -->
