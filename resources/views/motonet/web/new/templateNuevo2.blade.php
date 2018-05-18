537127caf49d7ec9986c3a3f1bef35f9ffc3bb84
Manuel Barrios <mbarrios@Manuels-MacBook-Pro.local>
2018-05-18 19:07:49 -0300

    nuevo todop

--- resources/views/motonet/web/new/templateNuevo2.blade.php
+++ resources/views/motonet/web/new/templateNuevo2.blade.php
@@ -157,7 +157,7 @@
     <div class="container">
 
         <div classs="col-xs-12">
-            <a href="{!! route('indexNueva') !!}#product-tabs-slider" class="dest"><img src="https://www.todopago.com.ar/sites/todopago.com.ar/files/mesa_de_trabajo_1_copia-100_0.jpg" alt="" width="100%" /></a>
+            <a href="{!! route('indexNueva') !!}#product-tabs-slider" class="dest"><img src="https://todopago.com.ar/sites/todopago.com.ar/files/mesa_de_trabajo_1_copia_9-80.jpg" alt="" width="100%" /></a>
         </div>
         <br>
           @yield('promos')
